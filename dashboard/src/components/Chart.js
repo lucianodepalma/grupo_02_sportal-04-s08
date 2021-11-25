ef141401ca7c593450a5e4733cc8e6ed9434dd2d
Hugo Vasili <vasili.hugo@gmail.com>
2021-11-25 06:02:26 -0300

    chart de ventas por mes activo

--- dashboard/src/components/Chart.js
+++ dashboard/src/components/Chart.js
@@ -5,7 +5,7 @@ const Chart = () => {
     
     return (
         <>
-            <iframe title="ventas" ariaLabel="Interactive line chart" id="datawrapper-chart-FjN8U" src="https://datawrapper.dwcdn.net/FjN8U/2/" scrolling="no" frameborder="0" width="100%" height="100%" />
+            <iframe title="ventas" aria-label="Interactive line chart" id="datawrapper-chart-FjN8U" src="https://datawrapper.dwcdn.net/FjN8U/2/" scrolling="no" frameBorder="0" width="100%" height="100%" />
 
 
         </>
